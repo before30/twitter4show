ddc517768573054d1eba585b76dde75355ead300
joseph <before30@gmail.com>
2010-11-16 23:10:09 +0900

    Server changed

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var sys = require('sys');
 	ws = require('./lib/ws');
 	events = require('events');
 	http = require('http');
-
+	url = require('url');
+	path = require('path');
+	fs = require('fs');
+	
 var httpClient = http.createClient(80, "search.twitter.com");
 var clients = [];
 
@@ -35,7 +38,7 @@ function getTweets(){
 } // end of getTweets
 
 //getTweets();
-//setInterval(getTweets, 10000);
+setInterval(getTweets, 10000);
 ws.createServer(function(websocket){
 	clients.push(websocket);
 	
@@ -49,3 +52,34 @@ ws.createServer(function(websocket){
 		}
 	});
 }).listen(8002);
+
+function load_static_file(uri, response){
+	var filename = path.join(process.cwd(), uri);
+	path.exists(filename, function(exists){
+		if(!exists){
+			response.writeHeader(404, {"Content-Type":"text/plain"});
+			response.write("404 Not Found\n");
+			response.end();
+			return;
+		}
+
+		fs.readFile(filename, "binary", function(err, file){
+			if(err){
+				response.writeHeader(500, {"Content-Type":"text/plain"});
+				response.write(err + "\n");
+				response.end();
+				return;
+			}
+
+			response.writeHeader(200);
+			response.write(file, "binary");
+			response.end();
+		});
+	});
+}
+
+http.createServer(function(request, response){
+	var uri = url.parse(request.url).pathname;
+	load_static_file(uri, response);
+}).listen(8888);
+
